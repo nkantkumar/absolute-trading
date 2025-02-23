a1b0184568a76d34e39fa40c9ead5bd8d39b5d1d
Kumar Nishikant <nkantkumar@gmail.com>
2025-02-24 00:28:04 +0800

    base code for match engine

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+mod match_engine;
+use match_engine::engine::{MatchingEngine, TradingPair};
+use match_engine::orderbook::{Order, OrderBook, BidOrAsk};
+use rust_decimal_macros::dec;
 fn main() {
-    println!("Hello, world!");
+    let buy_order = Order::new(BidOrAsk::Bid, 5.5);
+    let mut orderbook = OrderBook::new();  
+    orderbook.add_limit_order(dec!(4.4), buy_order);
+    //println!("{:?}", orderbook);
+    let mut engine = MatchingEngine::new();
+    let pair = TradingPair::new("BTC".to_string(), "USD".to_string());
+    engine.add_new_market(pair.clone());
+    let buy_order = Order::new(BidOrAsk::Bid, 5.5);
+    engine.place_limit_order(pair, dec!(10000), buy_order).unwrap();  
 }
